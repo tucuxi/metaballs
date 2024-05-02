79b83356630b219c1c727196c2654dd22f00b333
Mahmoud Kassab <mahmoud.k@hybridsolutions.com>
2024-05-02 20:20:02 +0300

    changing cyan color

--- internal/colors.go
+++ internal/colors.go
@@ -5,6 +5,6 @@ import "image/color"
 type BallColor color.Color
 var (
 	Pink = color.RGBA{196, 77, 86, 1}
-	Cyan = color.RGBA{0, 255, 255, 1}
-	Gray = color.RGBA{6, 24, 44,  1}
+	Cyan = color.RGBA{26, 107, 151, 1}
+	Gray = color.RGBA{128, 128, 128, 1}
 )
