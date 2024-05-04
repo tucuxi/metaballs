90d287867ea76473a7a38fe8ebcd27f1269efda8
Mahmoud Kassab <mahmoud.k@hybridsolutions.com>
2024-05-04 17:50:28 +0300

    improving performance using goroutine

--- internal/screen.go
+++ internal/screen.go
@@ -4,6 +4,7 @@ import (
 	"image"
 	"image/color"
 	"math"
+	"sync"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
@@ -57,42 +58,46 @@ func (s *Screen) StartPainter() {
 // Called when s.Refresh() is called
 // Re-render the screen
 func (s *Screen) draw(w, h int) image.Image {
-	// start := time.Now()
-	// fgcolor := theme.WarningColor()
 	img := image.NewRGBA(image.Rect(0, 0, int(s.Size().Height), int(s.Size().Width)))
 
 	size := float32(max(w, h))
 	g := int(math.Ceil(float64(size) / float64(s.resolution)))
-
+	wait := sync.WaitGroup{}
 	for row := 0; row < h; row += g {
 		y := float32(row) / size
 		y2 := float32(row+g) / size
+		wait.Add(1)
+		go func(row int) {
+			defer wait.Done()
 
-		for col := 0; col < w; col += g {
+			for col := 0; col < w; col += g {
 
-			x := float32(col) / size
-			x2 := float32(col+g) / size
+				x := float32(col) / size
+				x2 := float32(col+g) / size
 
-			a, b, c, d := s.group.valueV2(x, x2, y, y2)
-			sq := Square{
-				a: a,
-				b: b,
-				c: c,
-				d: d,
+				a, b, c, d := s.group.valueV2(x, x2, y, y2)
+				sq := Square{
+					a: a,
+					b: b,
+					c: c,
+					d: d,
+				}
+				sq.MarchV2(col, row, g, img, s.color)
+
+				// Too much time wasted on copying and looping through the lines
+				// lines := sq.March(col, row, g)
+				// for _, line := range lines {
+
+				// 	bresenham.DrawLine(img, line.x1, line.y1, line.x2, line.y2, color)
+				// }
+
+				// Too much time wasted on managing the goroutines
+				// go sq.MarchV3(col, row, g)
+
 			}
-			// lines := sq.March(col, row, g)
-			// for _, line := range lines {
-
-			// 	bresenham.DrawLine(img, line.x1, line.y1, line.x2, line.y2, color)
-			// }
-			// go sq.MarchV3(col, row, g)
-			sq.MarchV2(col, row, g, img, s.color)
-		}
+		}(row)
 
 	}
-
-	// elapsed := time.Since(start)
-	// fmt.Println("draw time: ", elapsed)
-
+	wait.Wait()
 	return img
 }
