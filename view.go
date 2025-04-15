b5a5da5e0ba77712b145b9a6410c846cb9ddb632
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2025-04-14 21:00:27 -0300

    Use fyne.DoAndWait

--- view.go
+++ view.go
@@ -32,7 +32,7 @@ func (mw *metaballsWidget) animate() {
 	go func() {
 		for range time.Tick(time.Millisecond * 40) {
 			mw.model.move()
-			mw.Refresh()
+			fyne.DoAndWait(mw.Refresh)
 		}
 	}()
 }
