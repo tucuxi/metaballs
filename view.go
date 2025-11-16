97d2999d4a44079a250ec1baeb1a39d3b056af32
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2025-11-16 11:09:57 +0100

    Refactor model rendering

--- view.go
+++ view.go
@@ -12,7 +12,10 @@ import (
 	"github.com/StephaneBunel/bresenham"
 )
 
-const iso = 1
+const (
+	iso    = 1
+	raster = 128
+)
 
 type metaballsWidget struct {
 	widget.BaseWidget
@@ -44,45 +47,45 @@ func (mw *metaballsWidget) CreateRenderer() fyne.WidgetRenderer {
 func (mw *metaballsWidget) draw(w, h int) image.Image {
 	fgcolor := theme.Color(theme.ColorNameForeground)
 	img := image.NewRGBA(image.Rect(0, 0, w, h))
-	size := float32(max(w, h))
-	g := int(math.Ceil(float64(size) / 128))
-	cols := w/g + 1 // +1 for the boundary
 
-	grid := make([][]float32, 2)
-	grid[0] = make([]float32, cols)
-	grid[1] = make([]float32, cols)
-	rowIndex := 0
+	size := min(h, w)
 
-	calcValue := func(i, j int) float32 {
-		x := float32(j*g) / size
-		y := float32(i*g) / size
-		return mw.model.value(x, y)
+	// dx and dy are the offsets to center the rendering area.
+	dx := float32(w-size) / 2
+	dy := float32(h-size) / 2
+
+	gridStep := float32(1) / raster
+
+	row1 := make([]float32, raster+1)
+	row2 := make([]float32, raster+1)
+
+	g := int(float32(size) / raster) // Cell size in pixels
+
+	for j := range row1 {
+		row1[j] = mw.model.value(float32(j)*gridStep, 0)
 	}
 
-	for j := range cols {
-		grid[0][j] = calcValue(0, j)
-	}
+	for i := range raster {
+		y := int(dy + float32(i)*float32(size)/raster)
 
-	for row := 0; row < h; row += g {
-		i := row / g
-		nextRowIndex := 1 - rowIndex
-
-		for j := range cols {
-			grid[nextRowIndex][j] = calcValue(i+1, j)
+		// Calculate the next row of values
+		for j := range raster + 1 {
+			row2[j] = mw.model.value(float32(j)*gridStep, float32(i+1)*gridStep)
 		}
 
-		for col := 0; col < w; col += g {
-			j := col / g
+		for j := range raster {
+			// Get values at the corners of the grid cell
+			a, b := row1[j], row1[j+1]
+			c, d := row2[j+1], row2[j]
 
-			a := grid[rowIndex][j]
-			b := grid[rowIndex][j+1]
-			c := grid[nextRowIndex][j+1]
-			d := grid[nextRowIndex][j]
+			// Calculate pixel coordinates for the left of the current grid cell and cell size
+			x := int(dx + float32(j)*float32(size)/raster)
 
-			a1, a2 := lerp(col, col+g, (iso-a)/(b-a)), row
-			b1, b2 := col+g, lerp(row, row+g, (iso-b)/(c-b))
-			c1, c2 := lerp(col, col+g, (iso-d)/(c-d)), row+g
-			d1, d2 := col, lerp(row, row+g, (iso-a)/(d-a))
+			// Calculate intersection points using linear interpolation
+			a1, a2 := lerp(x, x+g, (iso-a)/(b-a)), y   // Top edge
+			b1, b2 := x+g, lerp(y, y+g, (iso-b)/(c-b)) // Right edge
+			c1, c2 := lerp(x, x+g, (iso-d)/(c-d)), y+g // Bottom edge
+			d1, d2 := x, lerp(y, y+g, (iso-a)/(d-a))   // Left edge
 
 			switch state(a, b, c, d) {
 			case 1, 14:
@@ -91,25 +94,18 @@ func (mw *metaballsWidget) draw(w, h int) image.Image {
 				bresenham.DrawLine(img, b1, b2, c1, c2, fgcolor)
 			case 3, 12:
 				bresenham.DrawLine(img, b1, b2, d1, d2, fgcolor)
-			case 4:
+			case 4, 11:
 				bresenham.DrawLine(img, a1, a2, b1, b2, fgcolor)
-			case 5:
+			case 5, 10:
 				bresenham.DrawLine(img, a1, a2, d1, d2, fgcolor)
 				bresenham.DrawLine(img, b1, b2, c1, c2, fgcolor)
-			case 6:
+			case 6, 9:
 				bresenham.DrawLine(img, a1, a2, c1, c2, fgcolor)
 			case 7, 8:
 				bresenham.DrawLine(img, a1, a2, d1, d2, fgcolor)
-			case 9:
-				bresenham.DrawLine(img, a1, a2, c1, c2, fgcolor)
-			case 10:
-				bresenham.DrawLine(img, a1, a2, b1, b2, fgcolor)
-				bresenham.DrawLine(img, c1, c2, d1, d2, fgcolor)
-			case 11:
-				bresenham.DrawLine(img, a1, a2, b1, b2, fgcolor)
 			}
 		}
-		rowIndex = nextRowIndex
+		row1, row2 = row2, row1
 	}
 	return img
 }
